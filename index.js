9f84abe4f3bf4eeee5cd1ce772e494eb28d20323
Edzel <derz@elidragon.com>
2021-11-26 21:29:34 +0000

    fixed config support

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const fs = require("fs");
 var   handle = require("./loader.js");
-var   config = new require("./config/app.js");//(handle);
+var   config = new new require("./config/app.js")(handle);//(handle);
 
 // reading folder with modules:
 const modules = fs.readdirSync("./modules/");
 modules.forEach((module) => {
+	if( module.endsWith(".dis") ) return
 	handle.load(module);
 })
+handle.config.preinit()
 
 // post registration:
 modules.forEach((module) => {
+	if( module.endsWith(".dis") ) return
 	handle[module].init(handle.giveto(module));
 })
 
