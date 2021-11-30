d2f2d3e24e79cfe0f683dca489bf6a9bfde3372c
Edzel <derz@elidragon.com>
2021-11-30 19:19:19 +0000

    express submodule support

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const fs = require("fs");
 var   handle = require("./loader.js");
-var   config = new new require("./config/app.js")(handle);//(handle);
+var   config = new new require("./config/app.js")(handle);
+var   express = new new require("./express/app.js")(handle);
 
 // reading folder with modules:
 const modules = fs.readdirSync("./modules/");
+
 modules.forEach((module) => {
 	if( module.endsWith(".dis") ) return
 	handle.load(module);
 })
-handle.config.preinit()
+handle.config.preinit();  // config things
+handle.express.preinit(); // express initialisation
 
 // post registration:
 modules.forEach((module) => {
@@ -16,3 +19,4 @@ modules.forEach((module) => {
 	handle[module].init(handle.giveto(module));
 })
 
+handle.express.start();
